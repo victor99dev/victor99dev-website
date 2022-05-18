1b8d1ec1ce0b054f3e088f8297b9772018199c16
Victor Hugo <93715052+torugo99@users.noreply.github.com>
2022-05-18 15:23:01 -0300

    commit initial

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".input", {
+    strings:["Desenvolvedor.","Analista de Sistemas.","DBA.","Web Design.","UI/UX Design.","Design Gráfico.","Youtuber.","Streamer.","Freelancer."],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop:true
+});

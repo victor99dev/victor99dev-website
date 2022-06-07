ab9e170d816d42cd1b5b2a35a076841e531c131a
Victor Hugo <93715052+torugo99@users.noreply.github.com>
2022-06-07 09:21:18 -0300

    incremento

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.top').addClass("sticky");
+        }else{
+            $('.top').removeClass("sticky")
+        }
+        if(this.scrollY > 500){
+            $('.scrollup').addClass("show")
+        }else{
+            $('.scrollup').removeClass("show")
+        }
+    })
+});
+
+$('.scrollup').click(function(){
+    $('html').animate({scrollTop: 0});
+});
+
+$('.navlist-btn').click(function(){
+    $('.navlist').toggleClass("active");
+    $('.navlist-btn i').toggleClass("active");
+});
+
 var typed = new Typed(".input", {
     strings:["Desenvolvedor.","Analista de Sistemas.","DBA.","Web Design.","UI/UX Design.","Design Gráfico.","Youtuber.","Streamer.","Freelancer."],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop:true
+});
+
+var typed = new Typed(".input2", {
+    strings:["Victor Hugo.","Desenvolvedor.","Analista de Sistemas.","DBA.","Web Design.","UI/UX Design.","Design Gráfico.","Youtuber.","Streamer.","Freelancer."],
     typeSpeed: 60,
     backSpeed: 60,
     loop:true
